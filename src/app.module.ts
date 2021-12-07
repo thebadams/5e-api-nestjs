ad78c6fec5f36309f787f06d2a433809cad5b765
Brian Adams <bpeteradams@gmail.com>
2021-12-07 13:45:41 -0500

    Feature(featureModule): Added connection and model configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeaturesModule } from './features/features.module';
 
 @Module({
-  imports: [FeaturesModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://localhost:27017/5eAPI?readPreference=primary&ssl=false',
+    ),
+    FeaturesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FeaturesService } from './features.service';
 import { FeaturesController } from './features.controller';
+import { Feature, FeatureSchema } from './schemas/feature.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Feature.name, schema: FeatureSchema }]),
+  ],
   controllers: [FeaturesController],
-  providers: [FeaturesService]
+  providers: [FeaturesService],
 })
 export class FeaturesModule {}
