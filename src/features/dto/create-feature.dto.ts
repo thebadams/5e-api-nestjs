2981de7da0b385aa6a6b08dd939668d040c0cf39
Brian Adams <bpeteradams@gmail.com>
2021-12-07 13:44:41 -0500

    Feature(featuresModule): Set up create-feature.dto

--- src/features/dto/create-feature.dto.ts
+++ src/features/dto/create-feature.dto.ts
@@ -1,1 +1,9 @@
-export class CreateFeatureDto {}
+import { IsString } from 'class-validator';
+
+export class CreateFeatureDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+}
